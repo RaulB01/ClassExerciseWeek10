71dd1d3e120c5ebec34e90c9bf79aea47ddc5546
rdb24057 <rdb24057@an098060-ct103v.nvstu.edu>
2015-03-24 15:49:07 -0400

    init

--- src/ClassExcerciseWeek10.java
+++ src/ClassExcerciseWeek10.java
@@ -4,10 +4,10 @@ public class ClassExcerciseWeek10 {
 	enum Month {January, February, March, April, May, June, July, August, September, October, November, December}
 	public static void main(String[] args) {
 		Month [] choices ={Month.January ,Month.February, Month.March,Month.April,Month.May,Month.June,Month.July,Month.August,Month.September,Month.October,Month.November,Month.December };
-		boolean continueLoop = true;
-		while(continueLoop){
+
+		
 		Month select = (Month)JOptionPane.showInputDialog(null,"Select a month.","Month",JOptionPane.QUESTION_MESSAGE,null, choices,choices[0]);
-		if(select!=null){
+		while(select!=null){
 		switch (select){
 		case December: case January: case February:
 			JOptionPane.showMessageDialog(null, "Do you want to build a snow man?");
@@ -22,5 +22,5 @@ public class ClassExcerciseWeek10 {
 			JOptionPane.showMessageDialog(null, "Welcome to the foliage season!");
 			break;
 		}
-	} else continueLoop=false;
+
 }}}
